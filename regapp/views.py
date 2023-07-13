e480437136b465499902133aabe59862095e8087
saikondepati35 <saikondepati35@gmail.com>
2023-07-13 17:13:42 +0530

    commit2

--- regapp/views.py
+++ regapp/views.py
@@ -2,35 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Reg
-# Create your views here.
+from .forms import RegForm,LoginForm
 class Home(View):
     def get(self,request):
         return render(request,'home.html')
 class RegInput(View):
     def get(self,request):
-        return render(request,'input.html')
+        con_dic={"regform":RegForm()}
+        return render(request,'input.html',context=con_dic)
 class RegView(View):
     def post(self,request):
-        fn=request.POST["t1"]
-        ln=request.POST["t2"]
-        un=request.POST["t3"]
-        pwd=request.POST["t4"]
-        cpwd=request.POST["t5"]
-        email=request.POST["t6"]
-        mob=int(request.POST["t7"])
-        r1=Reg(firstname=fn,lastname=ln,username=un,password=pwd,cpassword=cpwd,emailid=email,mobileno=mob)
-        r1.save()
-        res=HttpResponse("reg success")
+        rf=RegForm(request.POST)
+        if rf.is_valid():
+            r1=Reg(firstname=rf.cleaned_data['firstname'],
+                   lastname=rf.cleaned_data["lastname"],
+                   username=rf.cleaned_data["username"],
+                   password=rf.cleaned_data["password"],
+                   cpassword=rf.cleaned_data["cpassword"],
+                   emailid=rf.cleaned_data["emailid"],
+                   mobileno=rf.cleaned_data["mobileno"]
+                   )
+            r1.save()
+            res=HttpResponse("reg success")
         return res
 class LoginInput(View):
     def get(self,request):
-        return render(request,'logininput.html')
+        condic={"loginform":LoginForm()}
+        return render(request,'logininput.html',context=condic)
 class Login(View):
     def post(self,request):
-        user=request.POST["un"]
-        pwd=request.POST["pw"]
-        objs=Reg.objects.filter(username=user,password=pwd)
-        if objs:
-            return HttpResponse("login success")
-        else:
-            return HttpResponse("login failed")
+        lf=LoginForm(request.POST)
+        if lf.is_valid():
+            objs=Reg.objects.filter(username=lf.cleaned_data["username"],password=lf.cleaned_data["password"])
+            if objs:
+                return HttpResponse("login success")
+            else:
+                return HttpResponse("login failed")
